2e223412aef629f0c762e0a35b041e19909d97bd
Curtis <tycur@protonmail.com>
2020-07-01 21:19:51 -0400

    This is the Fourth Exercise.
    New File = four.py

---
+++ four.py
@@ -0,0 +1,11 @@
+"""This is a mad lib program"""
+#!/usr/bin/env python3
+
+
+def main():
+    """This is a mad lib module"""
+    noun = str(input("Enter a noun: "))
+    verb = str(input("Enter a verb: "))
+    adjective = str(input("Enter a adjective: "))
+    adverb = str(input("Enter a adverb: "))
+    print("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? Thats dank.")
