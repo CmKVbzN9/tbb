a7907f44efde33fb12c5d0db82dc75f21be48694
Curtis <tycur@protonmail.com>
2020-07-01 23:28:05 -0400

    This is the Ninth Exercise
    New File = nine.py

---
+++ nine.py
@@ -0,0 +1,14 @@
+"""Paint Calculator"""
+#!/usr/bin/env python3
+
+
+def main():
+    """Paint Calculator Module"""
+    length = int(input("Length? "))
+    width = int(input("Width? "))
+    area = length * width 
+    paint = area / 350
+    print("You will need to purchase", paint, "gallons of paint to cover", area, "square feet.")
+
+if __name__ == '__main__':
+    main()
