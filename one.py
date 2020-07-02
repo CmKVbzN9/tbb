00cbc4c8529d09173a34ceaba5c3ed529697806a
Curtis <tycur@protonmail.com>
2020-07-01 21:06:43 -0400

    Initial commit for exercises
    New File: one.py

---
+++ one.py
@@ -0,0 +1,8 @@
+"""This is hello world"""
+#!/usr/bin/env python3
+
+
+def main():
+    """This is a hello world module"""
+    user_input = str(input("What is your name? "))
+    print("Hello, " + user_input + ", nice to meet you!")
