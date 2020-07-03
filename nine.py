6c2b1e7e346f96730c91c8b47d0a242b00afa00c
Curtis <tycur@protonmail.com>
2020-07-02 22:02:41 -0400

    Fixed extra white spaces
    Updated File = nine.py

--- nine.py
+++ nine.py
@@ -6,7 +6,7 @@ def main():
     """Paint Calculator Module"""
     length = int(input("Length? "))
     width = int(input("Width? "))
-    area = length * width 
+    area = length * width
     paint = area / 350
     print("You will need to purchase", paint, "gallons of paint to cover", area, "square feet.")
 
