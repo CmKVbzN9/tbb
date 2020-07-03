b35fd1cee15450028d59fe1aa60d80b5d340a1dd
Curtis <tycur@protonmail.com>
2020-07-02 22:00:16 -0400

    This is the Thirteenth Exercise
    New File = thirteen.py

---
+++ thirteen.py
@@ -0,0 +1,17 @@
+"""This program determines compound interest"""
+#!/usr/bin/env python3
+
+
+def main():
+    """This module determines compound interest"""
+    principle = int(input("What is the principal amount? "))
+    rate = float(input("What is the rate "))
+    years = int(input("What is the number of years? "))
+    times = int(input("How many times is the interest compounded per year? "))
+    interest = rate / 100
+    total_value = principle * (1 + interest / times) ** (times * years)
+    rounded_value = round(total_value, 2)
+    print('{}$ is invested at {}%, for {} years componded {} times per year is ${}.'.format(principle, rate, years, times, rounded_value))
+
+if __name__ == '__main__':
+    main()
