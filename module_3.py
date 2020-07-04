a2d39b3fda48b89e21f4851e18af3c176c582992
root <root@mountain.hsd1.md.comcast.net>
2020-07-04 10:17:58 -0500

    Updated module_3.py
    Fixed error

--- module_3.py
+++ module_3.py
@@ -6,7 +6,7 @@ def main():
     """This module creates quotes"""
     quote = str(input("What is the quote? "))
     said_by = str(input("Who said it? "))
-    print('{} says, "{}".'.format(quote, said_by))
+    print('{} says, "{}".'.format(said_by, quote))
 
 if __name__ == '__main__':
     main()
