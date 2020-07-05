86741469bec0135e977fd2b5453c4751b306ed76
CmKVbzN9 <67663263+CmKVbzN9@users.noreply.github.com>
2020-07-05 16:11:24 -0700

    Update module_5.py
    optimized output

--- module_5.py
+++ module_5.py
@@ -6,10 +6,10 @@ def main():
     """This module does simple math"""
     first_number = int(input("What is the first number? "))
     second_number = int(input("What is the second number? "))
-    print("{} + {}".format(first_number, second_number), " = ", first_number+second_number)
-    print("{} - {}".format(first_number, second_number), " = ", first_number-second_number)
-    print("{} * {}".format(first_number, second_number), " = ", first_number*second_number)
-    print("{} / {}".format(first_number, second_number), " = ", first_number/second_number)
+    print("{} + {} = {}".format(first_number, second_number, first_number + second_number))
+    print("{} - {} = {}".format(first_number, second_number, first_number - second_number))
+    print("{} * {} = {}".format(first_number, second_number, first_number * second_number))
+    print("{} / {} = {}".format(first_number, second_number, first_number / second_number))
 
 if __name__ == '__main__':
     main()
